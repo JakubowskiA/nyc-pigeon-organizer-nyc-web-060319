61a089e9db821cf5616ca1b6d8eed246cc15d824
Ariel J <arieljakubowski@gmail.com>
2019-06-04 02:35:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   new_hash = {}
 data.each do |property, attribute|
-  attribute.each do |detail, name|
+  attribute.each do |detail, names|
   binding.pry
 end
 end
